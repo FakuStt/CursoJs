68caf36697e343d315eb3a996afab8c8322a2b43
Facundo <facundo.stazione@gmail.com>
2023-10-15 18:37:21 -0300

    Segunda preentrega

--- main.js
+++ main.js
@@ -27,40 +27,122 @@ while (!correo.includes ("@") || !correo.includes (".com")) {
 }
 
 function enviarCorreo() {
-    let enviar = prompt(`Deseas que cuando haya algún descuento te enviemos un aviso al siguiente correo: ${correo} ?     SI / NO`)
-    if (enviar.toLowerCase() === "si") {
-        alert("¡Genial! A la brevedad enviaremos un correo con las ofertas y promociones. Mientras tanto, te invitamos a ver nuestros últimos modelos de macetas en nuestra página. ¡Gracias!")
-    } else if (enviar.toLowerCase() === "no") {
-        alert("De todos modos, te invitamos a ver nuestro catálogo. ¡Gracias!")
-    } else {
-        alert("Respuesta incorrecta. Por favor, responde con 'SI' o 'NO'.")
+    let enviar = "";
+    while(enviar.toLowerCase() !== "si" && enviar.toLowerCase() !== "no"){
+        enviar = prompt(`Deseas que cuando haya algún descuento te enviemos un aviso al siguiente correo: ${correo} ?     SI / NO`)
+        if (enviar.toLowerCase() === "si") {
+            alert("¡Genial! A la brevedad enviaremos un correo con las ofertas y promociones. Mientras tanto, te invitamos a ver nuestros últimos modelos de macetas en nuestra página. ¡Gracias!")
+        } else if (enviar.toLowerCase() === "no") {
+            alert("De todos modos, te invitamos a ver nuestro catálogo. ¡Gracias!")
+        } else {
+            alert("Respuesta incorrecta. Por favor, responde con 'SI' o 'NO'.")
+        }
     }
 }
 
-let buscar = prompt("¿Necesitas ayuda con algo? SI/NO");
+enviarCorreo();
 
-if (buscar.toLowerCase() === "si") {
-    let precio = prompt("¿Qué maceta te interesaría comprar? Chica / Mediana / Grande / Todas")
-    switch (precio.toLowerCase()) {
-        case "chica":
-            alert(`Las macetas chicas tienen un precio de $300, son de buena calidad y las personalizamos a tu gusto.`)
-            enviarCorreo();
-            break;
-        case "mediana":
-            alert(`Las macetas medianas tienen un precio inicial de $400, son de buena calidad y las personalizamos a tu gusto.`)
-            enviarCorreo();
-            break;
-        case "grande":
-            alert(`Las macetas grandes tienen un precio inicial de $450, son de buena calidad y las personalizamos a tu gusto.`)
-            enviarCorreo();
-            break;
-        case "todas":
-            alert(`Las macetas chicas están a $300; las macetas medianas desde $400 y las macetas grandes están a un precio inicial de $450.`)
-            enviarCorreo();
-            break;
-        default:
-            break;
+
+
+const productos = [
+    {id: 1, tamaño: "chica", precio: 300},
+    {id: 2, tamaño: "mediana", precio: 400},
+    {id: 3, tamaño: "grande", precio: 450}
+]
+
+let ayuda = "";
+let buscar = prompt("¿Necesitas ayuda con algo? SI/NO");
+let preguntar = "";
+let error = "";
+let buscarId = "";
+let buscarTamaño = "";
+let buscarPrecio = "";
+let resultado = "";
+let sizes = "";
+let costos = "";
+
+while (true) {
+    if (buscar.toLowerCase() === "si") {
+        while (true) {
+            ayuda = prompt("¿Con qué te podríamos ayudar? 1-BUSCAR MACETAS / 2-ERROR CON LA PÁGINA / 3-OTRO      SELECCIONE SOLO EL NÚMERO DE LA OPCIÓN");
+            if (ayuda === "1") {
+                preguntar = prompt("Buscar maceta por: 1- Id / 2- Tamaño / 3- Precio    SELECCIONA LA OPCIÓN POR EL NÚMERO");
+                if (preguntar === "1") {
+                    buscarId = parseInt(prompt("Ingresa el Id del producto que estas buscando. 1 / 2 / 3   SOLO INGRESE EL NUMERO"));
+                    if (buscarId === 1) {
+                        resultado = productos.filter((item) => item.id === 1);
+                    } else if (buscarId === 2) {
+                        resultado = productos.filter((item) => item.id === 2);
+                    } else if (buscarId === 3) {
+                        resultado = productos.filter((item) => item.id === 3);
+                    } else {
+                        alert("No seleccionaste una opcion de manera correcta. Aun asi, te mostraremos todas las opciones que tenemos disponible.");
+                        resultado = productos;
+                    }
+                    resultado.forEach((item) => {
+                        alert(`
+                        Id: ${item.id}
+                        Tamaño: ${item.tamaño}
+                        Precio: $${item.precio}
+                        `);
+                    });
+                } else if (preguntar === "2") {
+                    buscarTamaño = prompt("Ingrese el tamaño que está buscando. 1- Chico / 2- Mediano / 3- Grande   SOLO INGRESE EL NUMERO DE LA OPCION");
+                    if (buscarTamaño === "1") {
+                        sizes = productos.filter((item) => item.tamaño === "chica");
+                    } else if (buscarTamaño === "2") {
+                        sizes = productos.filter((item) => item.tamaño === "mediana");
+                    } else if (buscarTamaño === "3") {
+                        sizes = productos.filter((item) => item.tamaño === "grande");
+                    } else {
+                        alert("No seleccionaste una opción de manera correcta. Aun así, te mostraremos todas las opciones que tenemos disponibles.");
+                        sizes = productos;
+                    }
+                    sizes.forEach((item) => {
+                        alert(`
+                            Id: ${item.id}
+                            Tamaño: ${item.tamaño}
+                            Precio: $${item.precio}
+                            `);
+                    });
+                } else if (preguntar === "3") {
+                    buscarPrecio = prompt("Ingrese hasta cuanto esta dispuesto gastar.  INGRESE SOLO NUMEROS");
+                    if (buscarPrecio > "450") {
+                        costos = productos.filter((item) => item.precio <= "450");
+                    } else if (buscarPrecio > "400") {
+                        costos = productos.filter((item) => item.precio <= "400");
+                    } else if (buscarPrecio > "300") {
+                        costos = productos.filter((item) => item.precio <= "300");
+                    } else {
+                        alert("No seleccionaste una opción de manera correcta. Aun así, te mostraremos todas las opciones que tenemos disponibles.");
+                        costos = productos;
+                    }
+                    alert(`Con un presupuesto de $${buscarPrecio} te alcanza para los siguientes productos:`)
+                    costos.forEach((item) => {
+                        alert(`
+                            Id: ${item.id}
+                            Tamaño: ${item.tamaño}
+                            Precio: $${item.precio}
+                            `);
+                    });
+                }                
+                break;
+            } else if (ayuda === "2"){
+                error = prompt(`¡Te pedimos disculpas! Por aquí puedes enviarnos tu problema y a la brevedad lo estaremos solucionando. Nos pondremos en contacto contigo enviando un correo a ${correo}`);
+                break;
+            } else if (ayuda === "3"){
+                alert(`Por favor, comunícate con nosotros desde tu correo electrónico ${correo} a nuestro correo ojalamacetas@gmail.com. ¡Gracias!`);
+                break;
+            } else{
+                alert("No has seleccionado ninguna opción, vuelve a intentarlo.");
+            }
+        }        
+        break;
+    } else if (buscar.toLowerCase() === "no"){
+        alert("¡Gracias por visitar nuestra página! Te invitamos a explorar nuestro hermoso catálogo.");
+        break;
+    } else {
+        alert("¡Respuesta incorrecta! Vuelve a intentarlo!");
+        buscar = prompt("¿Necesitas ayuda con algo? SI/NO");
     }
-} else {
-    alert("¡Gracias por visitar nuestra página! Te invitamos a explorar nuestro hermoso catálogo.")
 }
