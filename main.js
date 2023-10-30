f0fc195e9dcc48892a9da32963782be0f692649d
Facundo <facundo.stazione@gmail.com>
2023-10-30 13:20:15 -0300

    agregamos DOM para tercera entrega

--- main.js
+++ main.js
@@ -1,148 +1,164 @@
-let usuario = ""
-let sexo = ""
+let usuario = "";
+let sexo = "";
+let correo = "";
+let enviar = "";
+let ayuda = "";
+let buscar = "";
 
-while (usuario === "" || (sexo.toLowerCase() !== "m" && sexo.toLowerCase() !== "f")) {
-    usuario = prompt("Ingrese su nombre de usuario")
-    sexo = prompt("Ingrese su sexo: M/F")
-
-    if (usuario != "" && sexo.toLowerCase() === "m") {
-        alert(`Bienvenido ${usuario}!`)
-    } else if (usuario != "" && sexo.toLowerCase() === "f"){
-        alert(`Bienvenida ${usuario}!`)
-    } else {
-        alert("Ingresaste mal tu nombre de usuario o tu sexo, vuelve a intentarlo!")
+function eliminar() {
+    while (document.body.firstChild) {
+        document.body.removeChild(document.body.firstChild);
     }
 }
 
-
-let correo = ""
-
-while (!correo.includes ("@") || !correo.includes (".com")) {
-    correo = prompt(`Ingrese el correo electronico vinculado al usuario: ${usuario}`)
-    if (correo.includes("@") && correo.includes(".com")) {
-    alert(`Tu dirección de correo electrónico se ha ingresado con éxito!`)
-    } else {
-        alert(`Dirección de correo electrónico incorrecta, vuelve a intentarlo!`)
-    }
-}
-
-function enviarCorreo() {
-    let enviar = "";
-    while(enviar.toLowerCase() !== "si" && enviar.toLowerCase() !== "no"){
-        enviar = prompt(`Deseas que cuando haya algún descuento te enviemos un aviso al siguiente correo: ${correo} ?     SI / NO`)
-        if (enviar.toLowerCase() === "si") {
-            alert("¡Genial! A la brevedad enviaremos un correo con las ofertas y promociones. Mientras tanto, te invitamos a ver nuestros últimos modelos de macetas en nuestra página. ¡Gracias!")
-        } else if (enviar.toLowerCase() === "no") {
-            alert("De todos modos, te invitamos a ver nuestro catálogo. ¡Gracias!")
-        } else {
-            alert("Respuesta incorrecta. Por favor, responde con 'SI' o 'NO'.")
+function botonSiguiente(callback) {
+    let boton = document.createElement("button");
+    boton.innerHTML = "Next";
+    document.body.appendChild(boton);
+    boton.addEventListener("click", () => {
+        eliminar();
+        if (typeof callback === "function") {
+            callback();
         }
-    }
+    });
 }
 
-enviarCorreo();
+function mensajeAlert(mensaje, callback) {
+    let parrafo = document.createElement("p");
+    parrafo.textContent = mensaje;
+    document.body.append(parrafo);
+    botonSiguiente(callback);
+}
 
+const mostrarPrompt = (mensajePrompt, valorPrompt, promptCallback) => {
+    let contenedorPrompt = document.createElement("div");
+    let label = document.createElement("label");
+    label.textContent = mensajePrompt;
+    let input = document.createElement("input");
+    input.type = "text";
+    input.value = valorPrompt;
+    let botonAceptar = document.createElement("button");
+    botonAceptar.textContent = "Accept";
+    let botonCancelar = document.createElement("button");
+    botonCancelar.textContent = "Cancel";
+    contenedorPrompt.append(label, input, botonAceptar, botonCancelar);
+    document.body.append(contenedorPrompt);
 
+    const cerrarPrompt = (valor) => {
+        document.body.removeChild(contenedorPrompt);
+        if (typeof promptCallback === "function") {
+            promptCallback(valor);
+        }
+    };
+
+    botonAceptar.addEventListener("click", () => {
+        cerrarPrompt(input.value);
+    });
+
+    botonCancelar.addEventListener("click", () => {
+        cerrarPrompt(null);
+    });
+};
+
+const functionUsuario = () => {
+    mostrarPrompt("Enter your username:", "", (valor) => {
+        usuario = valor;
+        const functionSexo = () => {
+            mostrarPrompt("Enter your gender: M/F", "", (valor) => {
+                sexo = valor.toLowerCase();
+                if (usuario !== "" && (sexo === "m" || sexo === "f")) {
+                    mensajeAlert(`Welcome ${usuario}!`, functionCorreo);
+                } else {
+                    mensajeAlert("You entered an incorrect username or gender. Please try again!", functionUsuario);
+                }
+            });
+        };
+        functionSexo();
+    });
+};
+
+const functionCorreo = () => {
+    mostrarPrompt(`Enter the email address associated with the username: ${usuario}`, "", (valor) => {
+        correo = valor;
+        if (correo.includes("@") && correo.includes(".com")) {
+            mensajeAlert("Your email address has been successfully entered!", enviarCorreo);
+        } else {
+            mensajeAlert("Incorrect email address, please try again!", functionCorreo);
+        }
+    });
+};
+
+const enviarCorreo = () => {
+    mostrarPrompt(`Do you want to receive discount alerts to the following email: ${correo}? YES / NO`, "", (valor) => {
+        enviar = valor.toLowerCase();
+        if (enviar === "yes") {
+            mensajeAlert("Great! We will send you an email with offers and promotions soon. In the meantime, feel free to browse our latest collection of pots on our website. Thank you!", functionAyuda);
+        } else if (enviar === "no") {
+            mensajeAlert("Nevertheless, we invite you to explore our catalog. Thank you!", functionAyuda);
+        } else {
+            mensajeAlert("Incorrect response. Please respond with 'YES' or 'NO'.", enviarCorreo);
+        }
+    });
+};
+
+const functionAyuda = () => {
+    mostrarPrompt("Do you need assistance with anything? YES/NO", "", (valor) => {
+        ayuda = valor.toLowerCase();
+        if (ayuda === "yes") {
+            functionBuscar();
+        } else if (ayuda === "no") {
+            mensajeAlert("Thank you for visiting our page! We invite you to explore our beautiful catalog.");
+        } else {
+            mensajeAlert("Incorrect response! Please try again!", functionAyuda);
+        }
+    });
+};
+
+const functionBuscar = () => {
+    mostrarPrompt("How can we assist you? 1-SEARCH POTS / 2-PAGE ERROR / 3-OTHER   SELECT ONLY THE OPTION NUMBER", "", (valor) => {
+        const opcion = valor.toLowerCase();
+        if (opcion === "1") {
+            buscarPorId();
+        } else if (opcion === "2") {
+            reportarError();
+        } else if (opcion === "3") {
+            mensajeAlert(`Please contact us from your email address ${correo} to our email ojalamacetas@gmail.com. Thank you!`);
+        } else {
+            mensajeAlert("You did not select any option. Please try again.", functionBuscar);
+        }
+    });
+};
+
+const buscarPorId = () => {
+    mostrarPrompt("Enter the Id of the product you are looking for. 1 / 2 / 3   ENTER ONLY THE NUMBER", "", (valor) => {
+        const buscarId = parseInt(valor);
+        const resultado = productos.filter((item) => item.id === buscarId);
+        if (resultado.length > 0) {
+            resultado.forEach((item) => {
+                mensajeAlert(`
+                Id: ${item.id}
+                Size: ${item.tamaño}
+                Price: $${item.precio}
+                `, functionBuscar);
+            });
+        } else {
+            mensajeAlert("You did not select a valid Id. Please try again.", buscarPorId);
+        }
+    });
+};
+
+const reportarError = () => {
+    mostrarPrompt(`We apologize for the inconvenience! You can report your problem here, and we will resolve it as soon as possible. We will contact you by sending an email to ${correo}`, "", (valor) => {
+        const error = valor;
+        // Process the error report
+        mensajeAlert("Thank you for reporting the issue. We will resolve it as soon as possible.", functionAyuda);
+    });
+};
 
 const productos = [
-    {id: 1, tamaño: "chica", precio: 300},
-    {id: 2, tamaño: "mediana", precio: 400},
-    {id: 3, tamaño: "grande", precio: 450}
-]
+    { id: 1, tamaño: "small", precio: 300 },
+    { id: 2, tamaño: "medium", precio: 400 },
+    { id: 3, tamaño: "large", precio: 450 }
+];
 
-let ayuda = "";
-let buscar = prompt("¿Necesitas ayuda con algo? SI/NO");
-let preguntar = "";
-let error = "";
-let buscarId = "";
-let buscarTamaño = "";
-let buscarPrecio = "";
-let resultado = "";
-let sizes = "";
-let costos = "";
-
-while (true) {
-    if (buscar.toLowerCase() === "si") {
-        while (true) {
-            ayuda = prompt("¿Con qué te podríamos ayudar? 1-BUSCAR MACETAS / 2-ERROR CON LA PÁGINA / 3-OTRO      SELECCIONE SOLO EL NÚMERO DE LA OPCIÓN");
-            if (ayuda === "1") {
-                preguntar = prompt("Buscar maceta por: 1- Id / 2- Tamaño / 3- Precio    SELECCIONA LA OPCIÓN POR EL NÚMERO");
-                if (preguntar === "1") {
-                    buscarId = parseInt(prompt("Ingresa el Id del producto que estas buscando. 1 / 2 / 3   SOLO INGRESE EL NUMERO"));
-                    if (buscarId === 1) {
-                        resultado = productos.filter((item) => item.id === 1);
-                    } else if (buscarId === 2) {
-                        resultado = productos.filter((item) => item.id === 2);
-                    } else if (buscarId === 3) {
-                        resultado = productos.filter((item) => item.id === 3);
-                    } else {
-                        alert("No seleccionaste una opcion de manera correcta. Aun asi, te mostraremos todas las opciones que tenemos disponible.");
-                        resultado = productos;
-                    }
-                    resultado.forEach((item) => {
-                        alert(`
-                        Id: ${item.id}
-                        Tamaño: ${item.tamaño}
-                        Precio: $${item.precio}
-                        `);
-                    });
-                } else if (preguntar === "2") {
-                    buscarTamaño = prompt("Ingrese el tamaño que está buscando. 1- Chico / 2- Mediano / 3- Grande   SOLO INGRESE EL NUMERO DE LA OPCION");
-                    if (buscarTamaño === "1") {
-                        sizes = productos.filter((item) => item.tamaño === "chica");
-                    } else if (buscarTamaño === "2") {
-                        sizes = productos.filter((item) => item.tamaño === "mediana");
-                    } else if (buscarTamaño === "3") {
-                        sizes = productos.filter((item) => item.tamaño === "grande");
-                    } else {
-                        alert("No seleccionaste una opción de manera correcta. Aun así, te mostraremos todas las opciones que tenemos disponibles.");
-                        sizes = productos;
-                    }
-                    sizes.forEach((item) => {
-                        alert(`
-                            Id: ${item.id}
-                            Tamaño: ${item.tamaño}
-                            Precio: $${item.precio}
-                            `);
-                    });
-                } else if (preguntar === "3") {
-                    buscarPrecio = prompt("Ingrese hasta cuanto esta dispuesto gastar.  INGRESE SOLO NUMEROS");
-                    if (buscarPrecio > "450") {
-                        costos = productos.filter((item) => item.precio <= "450");
-                    } else if (buscarPrecio > "400") {
-                        costos = productos.filter((item) => item.precio <= "400");
-                    } else if (buscarPrecio > "300") {
-                        costos = productos.filter((item) => item.precio <= "300");
-                    } else {
-                        alert("No seleccionaste una opción de manera correcta. Aun así, te mostraremos todas las opciones que tenemos disponibles.");
-                        costos = productos;
-                    }
-                    alert(`Con un presupuesto de $${buscarPrecio} te alcanza para los siguientes productos:`)
-                    costos.forEach((item) => {
-                        alert(`
-                            Id: ${item.id}
-                            Tamaño: ${item.tamaño}
-                            Precio: $${item.precio}
-                            `);
-                    });
-                }                
-                break;
-            } else if (ayuda === "2"){
-                error = prompt(`¡Te pedimos disculpas! Por aquí puedes enviarnos tu problema y a la brevedad lo estaremos solucionando. Nos pondremos en contacto contigo enviando un correo a ${correo}`);
-                break;
-            } else if (ayuda === "3"){
-                alert(`Por favor, comunícate con nosotros desde tu correo electrónico ${correo} a nuestro correo ojalamacetas@gmail.com. ¡Gracias!`);
-                break;
-            } else{
-                alert("No has seleccionado ninguna opción, vuelve a intentarlo.");
-            }
-        }        
-        break;
-    } else if (buscar.toLowerCase() === "no"){
-        alert("¡Gracias por visitar nuestra página! Te invitamos a explorar nuestro hermoso catálogo.");
-        break;
-    } else {
-        alert("¡Respuesta incorrecta! Vuelve a intentarlo!");
-        buscar = prompt("¿Necesitas ayuda con algo? SI/NO");
-    }
-}
+functionUsuario();
